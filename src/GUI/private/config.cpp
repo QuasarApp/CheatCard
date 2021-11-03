80c7be0e2ba9166cbf862e5cf320796850c038c6
EndrII <EndrIIMail@gmail.com>
2021-11-03 11:07:27 +0300

    update license

--- src/GUI/private/config.cpp
+++ src/GUI/private/config.cpp
@@ -1,6 +1,6 @@
 //#
 //# Copyright (C) 2021-2021 QuasarApp.
-//# Distributed under the lgplv3 software license, see the accompanying
+//# Distributed under the GPLv3 software license, see the accompanying
 //# Everyone is permitted to copy and distribute verbatim copies
 //# of
 
