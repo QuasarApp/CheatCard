334ddf8412e14bd95bc3ab027fc7d576112e8ce3
EndrII <EndrIIMail@gmail.com>
2021-11-18 23:24:07 +0300

    ref #111 db fixes

--- src/RC/private/sql/SQLPatch_2.sql
+++ src/RC/private/sql/SQLPatch_2.sql
@@ -1,14 +1,38 @@
--- Adds colorTheme, clientName, cameraDevice for card:
+-- Adds colorTheme, shareName, cameraDevice for card:
 -- See task 111
 -- https://quasarapp.ddns.net:3000/QuasarApp/CheatCard/issues/111
 
 BEGIN TRANSACTION;
 
-ALTER TABLE Config ADD COLUMN colorTheme TEXT DEFAULT NULL;
-ALTER TABLE Config ADD COLUMN clientName TEXT DEFAULT NULL;
-ALTER TABLE Config ADD COLUMN cameraDevice TEXT DEFAULT NULL;
-ALTER TABLE Config ADD COLUMN devSettingEnable TEXT DEFAULT NULL;
-ALTER TABLE Config ADD COLUMN host TEXT DEFAULT NULL;
-ALTER TABLE Config ADD COLUMN port TEXT DEFAULT NULL;
+CREATE TABLE "Config_tmp" (
+    "user"   INTEGER NOT NULL UNIQUE,
+    "fFirst" BOOLEAN DEFAULT false,
+    "fSellerMode" BOOLEAN DEFAULT true,
+
+    FOREIGN KEY(user) REFERENCES Users(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+);
+
+INSERT INTO Config_tmp SELECT user,fFirst,fSellerMode FROM Config;
+DROP TABLE Config;
+CREATE TABLE IF NOT EXISTS "Config" (
+    "user"   INTEGER NOT NULL UNIQUE,
+    "fFirst" BOOLEAN DEFAULT NULL,
+    "fSellerMode" BOOLEAN DEFAULT NULL,
+    "colorTheme" TEXT DEFAULT NULL,
+    "shareName" BOOLEAN DEFAULT NULL,
+    "cameraDevice" TEXT DEFAULT NULL,
+    "devSettingEnable" BOOLEAN DEFAULT NULL,
+    "host" TEXT DEFAULT NULL,
+    "port" TEXT DEFAULT NULL,
+
+    FOREIGN KEY(user) REFERENCES Users(id)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+);
+
+INSERT INTO Config(user,fFirst,fSellerMode) SELECT user,fFirst,fSellerMode FROM Config_tmp;
+DROP TABLE Config_tmp;
 
 COMMIT;
