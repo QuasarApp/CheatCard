ee861058660fa9117ad38047b5c22dd54194c5ff
EndrII <EndrIIMail@gmail.com>
2022-07-09 18:14:37 +0300

    ref #511 added more logs

--- src/RC/CheatCard/api/apiv1.cpp
+++ src/RC/CheatCard/api/apiv1.cpp
@@ -111,12 +111,17 @@ bool ApiV1::processCardStatusImpl(const QH::PKG::DataPack<APIv1::UsersCards> &ca
 
         // ignore seels statuses that has a depricated time.
         if (dbUsersCards && dbUsersCards->getTime() > cardStatus->getTime()) {
+            QuasarAppUtils::Params::log(QString("Receive deprecated cards seal"
+                                        " Current seal time: %0 receiverd seal time: %1").
+                                        arg(dbUsersCards->getTime().toSecsSinceEpoch()).
+                                        arg(cardStatus->getTime().toSecsSinceEpoch()));
             continue;
         }
 
         if (!cardValidation(dbCard, cardStatuses.customData())) {
 
-            QuasarAppUtils::Params::log("Receive not signed cards seal");
+            QuasarAppUtils::Params::log("Receive not signed cards seal",
+                                        QuasarAppUtils::Warning);
             break;
         }
 
