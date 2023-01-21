b05b0fb2033c94af7438843204c055eebc99a930
EndrII <EndrIIMail@gmail.com>
2023-01-21 21:29:30 +0100

    squash all database patches fix #610

--- src/DB/private/sql/SQLPatch_1.sql
+++
@@ -1,9 +0,0 @@
--- Adds version for card:
--- See task 130
--- https://quasarapp.ddns.net:3000/QuasarApp/CheatCard/issues/130
-
-BEGIN TRANSACTION;
-
-ALTER TABLE Cards ADD COLUMN cardVersion INT DEFAULT 0;
-
-COMMIT;
