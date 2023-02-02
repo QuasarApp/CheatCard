3540ac7a489e8475ec07ec567d6400d0c047e531
EndrII <EndrIIMail@gmail.com>
2023-02-02 14:39:23 +0100

    finsihe works with api interface

--- src/Interfaces/public/rci/core/iapi.h
+++ src/Interfaces/public/rci/core/iapi.h
@@ -10,7 +10,6 @@
 
 #include "rci/global.h"
 #include "rci/objects/icard.h"
-#include "rci/objects/isession.h"
 #include "rci/objects/icontacts.h"
 #include "rci/objects/iuserscards.h"
 
@@ -35,34 +34,13 @@ public:
     virtual ~iAPI() = default;
 
     /**
-     * @brief sendCardStatusRequest This method should be send request to get status of my account
-     * @param userSession this is session id.
-     * @param dist This is distanation node.
-     */
-    virtual void sendCardStatusRequest(long long userSession,
-                                       QH::AbstractNodeInfo *dist) = 0;
-
-    /**
-     * @brief sendSessions This method send session to server
-     * @param sessionsthis is session data.
-     * @param dist This is distanation node.
-     */
-    virtual void sendSessions(const QHash<long long, QSharedPointer<Interfaces::iSession> > &sessions,
-                              QH::AbstractNodeInfo *dist) = 0;
-
-    /**
-     * @brief restoreOldDateRequest This is request for recavery users data froma server backup
+     * @brief syncRequest This is request for recavery users data froma server backup
      * @param curentUserKey this is user key
      * @param dist This is distanation node.
      */
-    virtual void restoreOldDateRequest(const QByteArray &curentUserKey, QH::AbstractNodeInfo *dist) = 0;
-
-    /**
-     * @brief restoreOneCardRequest This is request to restore one card data.
-     * @param cardId this is card id that will be restored.
-     * @param dist This is distanation node.
-     */
-    virtual void restoreOneCardRequest(unsigned int cardId, QH::AbstractNodeInfo *dist) = 0;
+    virtual void syncRequest(const QByteArray &curentUserKey,
+                             QH::AbstractNodeInfo *dist,
+                             const std::function<void(bool successful)>& = {}) = 0;
 
     /**
      * @brief sendContacts This method send to server new information about contacts
@@ -73,9 +51,10 @@ public:
      * @return true if request created and sent successful else false.
      */
     virtual bool sendContacts(const iContacts& conntact,
-                       const QByteArray& secreet,
-                       bool removeRequest,
-                       QH::AbstractNodeInfo *dist) = 0;
+                              const QByteArray& secreet,
+                              bool removeRequest,
+                              QH::AbstractNodeInfo *dist,
+                              const std::function<void(int err)>& = {}) = 0;
 
     /**
      * @brief deleteCard This is delete card request
@@ -86,7 +65,8 @@ public:
      */
     virtual bool deleteCard(unsigned int cardId,
                             const QByteArray &curentUserKey,
-                            QH::AbstractNodeInfo *dist) = 0;
+                            QH::AbstractNodeInfo *dist,
+                            const std::function<void(int err)>& = {}) = 0;
 
     /**
      * @brief sendUpdateCard This request send new updated information about the card.
@@ -95,7 +75,10 @@ public:
      * @param dist This is distanation node.
      * @return true if request created and sent successful else false.
      */
-    virtual bool sendUpdateCard(unsigned int cardId, unsigned int version, QH::AbstractNodeInfo *dist) = 0;
+    virtual bool sendUpdateCard(const QByteArray& cardId,
+                                unsigned int version,
+                                QH::AbstractNodeInfo *dist,
+                                const std::function<void(int err)>& = {}) = 0;
 
     /**
      * @brief changeUsersData This request send new updated information about users purcheses.
@@ -103,19 +86,18 @@ public:
      * This user must have a access to card.
      * @param cardId card id
      * @param userId user id
-     * @param session is session key.
      * @param purchasesCount this is cout of purshise of user
      * @param receivedCount this is cout of received bonuses of user on this card with @a cardId.
      * @param dist This is distanation node.
      * @return true if request created and sent successful else false.
      */
     virtual bool changeUsersData(const QByteArray& sellerUserKey,
-                                 unsigned int cardId,
-                                 unsigned int userId,
-                                 unsigned long long session,
+                                 const QByteArray& cardId,
+                                 const QByteArray& userId,
                                  unsigned int purchasesCount,
                                  unsigned int receivedCount,
-                                 QH::AbstractNodeInfo *dist) = 0;
+                                 QH::AbstractNodeInfo *dist,
+                                 const std::function<void(int err, const QSharedPointer<Interfaces::iUsersCards>& currentState)>& = {}) = 0;
 
 signals:
     /**
@@ -131,18 +113,10 @@ signals:
     virtual void sigCardReceived(QSharedPointer<RC::Interfaces::iCard>) = 0;
 
     /**
-     * @brief sigSessionStatusResult This signal will emit when client receive responce of changes from server
-     * @param succesed This is result.
+     * @brief sigCardRemoved This signal will be envoked when card will be removed.
+     * @param cardId This is sha256 card id.
      */
-    virtual void sigSessionStatusResult(QSharedPointer<RC::Interfaces::iSession>, bool succesed) = 0;
-
-    /**
-     * @brief sigContactsStatusResult This signal will emit when a client receive responce from server about contact.
-     * @param contact This is result of contact object.
-     * @param succesed this is result
-     * @param removed This will be true when a contact was removed.
-     */
-    virtual void sigContactsStatusResult(QSharedPointer<RC::Interfaces::iContacts> contact, bool succesed, bool removed) = 0;
+    virtual void sigCardRemoved(const QByteArray& cardId) = 0;
 
     /**
      * @brief sigContactsListChanged This signal will emit when contacts list was changed.
